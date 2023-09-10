caf9b6c523be8532bde9bfb5db634793f2c31ad1
O852471107 <140432390+O852471107@users.noreply.github.com>
2023-09-10 20:37:49 +0700

    HEC.cpp

--- HEC.cpp
+++ HEC.cpp
@@ -11,3 +11,8 @@ int main(){
     string answer = HEC[Rand(0, 3)];
     cout << "apply " << answer << endl;
 }
+// instructions: run the code and apply for the department revealed in the output
+// if you don't know how to run the code:
+// 1. access https://www.onlinegdb.com/online_c++_compiler
+// 2. run the code
+// 3. again, apply for the department revealed in the output
